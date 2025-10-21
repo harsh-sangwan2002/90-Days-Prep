5dedbb23e4f50b59c938247323a506e4ba496961
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-10-21 11:36:45 +0530

    updated App.jsx

--- React/08-react-interview-prep/src/App.jsx
+++ React/08-react-interview-prep/src/App.jsx
@@ -1,36 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-=======
 import './App.css'
 
 function App() {
@@ -55,7 +23,6 @@ function App() {
         }
       </ul>
     </div>
->>>>>>> d102f26edf5096a40fc5dc2ce6735561a97a614b
   )
 }
 
