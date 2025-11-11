7d4ca744f2cde8800a0d08f282f0c454e4ed05f0
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-11-11 12:42:09 +0530

    implemented my own server

---
+++ Node/YouTube/03-OwnServer/server.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<h1>Hello, World!</h1>');
+    // res.end();
+    let path = './views';
+
+    switch (req.url) {
+        case '/':
+            path += '/index.html';
+            res.statusCode = 200; // OK
+            break;
+        case '/about':
+            path += '/about.html';
+            res.statusCode = 200; // OK
+            break;
+        case '/about-me':
+            path += '/about.html';
+            res.statusCode = 301; // Redirected Permanently
+            res.setHeader('Location', '/about');
+            break;
+        default:
+            path += '/404.html';
+            res.statusCode = 404; // Not Found
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+
+        if (err)
+            console.log(err);
+
+        else {
+            res.end(data);
+        }
+    });
+})
+
+// localhost is a loopback address that points to the local machine
+// ports are used to differentiate between multiple services running on the same machine
+server.listen(3000, 'localhost', () => {
+    console.log("Backend server is running on port 3000");
+})
