711b011cee0609a5ad2acf71d84e4e3ce3393e7c
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-11-12 12:41:26 +0530

    removed interview project

--- Interview/dws/server/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { registerUser, loginUser, logoutUser, protect } = require('../controllers/user.controller');
-
-router.post('/register', registerUser)
-    .post('/login', loginUser)
-    .post('/logout', protect, logoutUser);
-
-module.exports = router;
