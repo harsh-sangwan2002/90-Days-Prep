d04f3c63a97eb406ad6d301e7fb48c78eb074246
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-11-12 15:57:16 +0530

    01-intro to react done

--- React/01-intro-to-react/src/components/TodoList.jsx
+++ React/01-intro-to-react/src/components/TodoList.jsx
@@ -1,27 +1,30 @@
 const Heading = ({ title, color }) => {
-    return <h1 style={{ color: color }}>{title}</h1>
+    console.log(color);
+    return (
+        <h1 style={{ color: color }}>{title}</h1>
+    )
 }
-
 const List = () => {
-    return <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>Item 5</li>
-    </ul>
+    return (
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+    )
 }
-
 const Summary = () => {
-    return <h3>Summary of Todo List</h3>
+    return (
+        <h3>Summary</h3>
+    )
 }
 
 const TodoList = () => {
     return (
         <div>
-            <Heading title="First Heading" color="blue" />
-            <Heading title="Second Heading" color="green" />
-            <List />
+            <Heading title="First Heading" color="red" />
+            <Heading title="Second Heading" color="blue" />
             <List />
             <Summary />
         </div>
