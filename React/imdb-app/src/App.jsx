a6cd0f3f738db4d273dca2df9d4e2049bcf30fe0
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-11-15 17:34:00 +0530

    fixed minor bugs

--- React/imdb-app/src/App.jsx
+++ React/imdb-app/src/App.jsx
@@ -3,24 +3,20 @@ import MovieListPage from './pages/MovieListPage'
 import MovieDetailsPage from './pages/MovieDetailsPage'
 import WatchListPage from './pages/WatchListPage'
 import Navbar from './components/Navbar'
-import { useState } from 'react'
 import WatchListProvider from './context/WatchlistContext'
 import { Provider } from 'react-redux'
 import store from './redux/Store'
 
 const App = () => {
-
-  const [watchlist, setWatchlist] = useState({});
-
   return (
     <Router>
       <Provider store={store}>
-        <WatchListProvider value={{ watchlist, setWatchlist }}>
+        <WatchListProvider>
           <Navbar />
           <Routes>
-            <Route path="/" element={<MovieListPage watchlist={watchlist} setWatchlist={setWatchlist} />} />
+            <Route path="/" element={<MovieListPage />} />
             <Route path="/details" element={<MovieDetailsPage />} />
-            <Route path="/watchlist" element={<WatchListPage watchlist={watchlist} setWatchlist={setWatchlist} />} />
+            <Route path="/watchlist" element={<WatchListPage />} />
           </Routes>
         </WatchListProvider>
       </Provider>
