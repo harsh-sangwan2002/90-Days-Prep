6ea34e3c9e9e91992e56ea1c72bbb3ba6fcc9a8b
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-11-27 09:07:05 +0530

    removed old classes of node.js

--- Node/Scaler/03-Intro-To-MongoDB-CRUD/routes/blog.route.js
+++
@@ -1,9 +0,0 @@
-const { getAllBlogs, getBlogById, createBlog, deleteBlog } = require('../controllers/blog.controller');
-const router = require('express').Router();
-
-router.get('/', getAllBlogs)
-    .get('/:id', getBlogById)
-    .post('/', createBlog)
-    .delete('/:id', deleteBlog);
-
-module.exports = router;
--- Node/Scaler/03-Intro-To-MongoDB-CRUD/utils/connectToDB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectToDB = async () => {
-    await mongoose.connect('mongodb+srv://hsangwan2002:Samay%40229@cluster0.18c099y.mongodb.net/');
-    console.log("Connected to MongoDB successfully");
-}
-
-module.exports = connectToDB;
