ab7749d267c72610354bd5f6d4bb39e93ef54017
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-12-06 10:38:07 +0530

    class 04- for loops done

---
+++ DSA/Refresher/04-For-Loop/Factors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factors {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                System.out.println(i);
+        }
+
+        scn.close();
+    }
+}
---
+++ DSA/Refresher/04-For-Loop/Print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Print {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 1; i <= n; i++)
+            System.out.println(i);
+
+        scn.close();
+    }
+}
