eecab0ccd9d800c5f1b6b6e511ea90157101fcaa
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-12-08 21:10:00 +0530

    Class 01 and 02 completed - Intro To OOPS and LLD

---
+++ Java/src/IntroToOOPS/BeginnerStudent.java
@@ -0,0 +1,10 @@
+package IntroToOOPS;
+
+public class BeginnerStudent extends Student{
+
+   BeginnerStudent(){
+       Student st = new Student();
+       // Cannot access email here
+//       st.email = "hsangwan2002@gmail.com";
+   }
+}
---
+++ Java/src/IntroToOOPS/Student.java
@@ -0,0 +1,18 @@
+package IntroToOOPS;
+
+public class Student {
+
+    public String name;
+    protected  int age;
+    String batchName; // default
+    private double psp;
+    private String email;
+
+    void attendClass(){
+        System.out.println("Student is attending class");
+    }
+
+    void bookMockInterview(){
+        System.out.println("Student is booked for mock interview");
+    }
+}
