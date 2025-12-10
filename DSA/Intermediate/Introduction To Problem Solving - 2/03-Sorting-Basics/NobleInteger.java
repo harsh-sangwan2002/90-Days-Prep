121b48cee64da7a20cbd845cc3d928b9e1b5e0e4
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-12-10 15:42:20 +0530

    class 03 - Sorting basics done

---
+++ DSA/Intermediate/Introduction To Problem Solving - 2/03-Sorting-Basics/NobleInteger.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+// TC -> O(N*log(N))
+// SC -> O(1)
+public class NobleInteger {
+
+    private void reverse(int[] A) {
+
+        int lo = 0, hi = A.length - 1;
+
+        while (lo < hi) {
+            int temp = A[lo];
+            A[lo] = A[hi];
+            A[hi] = temp;
+
+            lo++;
+            hi--;
+        }
+    }
+
+    public int solve(int[] A) {
+
+        Arrays.sort(A);
+        reverse(A);
+
+        if (A[0] == 0)
+            return 1;
+
+        int count = 0;
+
+        for (int i = 1; i < A.length; i++) {
+
+            if (A[i] != A[i - 1])
+                count = i;
+
+            if (count == A[i])
+                return 1;
+        }
+
+        return -1;
+    }
+}
