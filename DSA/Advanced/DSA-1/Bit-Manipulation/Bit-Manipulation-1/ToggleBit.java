a2479057e8206b1a106c7dd9800ba95b2e48ba0c
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-12-16 17:32:00 +0530

    Advanced DSA - OneD completed

--- DSA/Advanced/DSA-1/Bit-Manipulation/Bit-Manipulation-1/ToggleBit.java
+++
@@ -1,9 +0,0 @@
-// TC -> O(1)
-// SC -> O(1)
-public class ToggleBit {
-
-    public int solve(int A, int B) {
-
-        return (A ^ (1 << B));
-    }
-}
--- DSA/Advanced/DSA-1/Maths/Modular-Arithmetic-And-GCD/ABModulo.java
+++
@@ -1,9 +0,0 @@
-// TC -> O(1)
-// SC -> O(1)
-public class ABModulo {
-
-    public int solve(int A, int B) {
-
-        return Math.abs(A - B);
-    }
-}
