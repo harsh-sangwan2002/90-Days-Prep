a60a24783a555e2a051a942a0718865a4f6294a7
harsh-sangwan2002 <hsangwan2002@gmail.com>
2025-12-28 18:12:48 +0530

    class 05 - carry forward and subarrays done

---
+++ DSA/Intermediate/Introduction To Problem Solving - 1/05-Carry-Forward-And-Subarrays/PickFromBothSides/sol2.java
@@ -0,0 +1,29 @@
+package PickFromBothSides;
+
+// TC -> O(N)
+// SC -> O(1)
+public class sol2 {
+
+    public int solve(int[] A, int B) {
+
+        int sum = 0;
+
+        for (int i = 0; i < B; i++)
+            sum += A[i];
+
+        int max = sum;
+
+        int j = A.length - 1, i = B - 1;
+
+        while (i >= 0) {
+            sum -= A[i];
+            sum += A[j];
+            max = Math.max(sum, max);
+
+            j--;
+            i--;
+        }
+
+        return max;
+    }
+}
