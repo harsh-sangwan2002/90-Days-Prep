c0aa2f51c3463319b1e5c9c335de37eb7c2f1791
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-01-29 21:39:23 +0530

    class02 - Code Execution and Hoisting done

--- JavaScript/Scaler/02-Code-Execution-Hoisting/arrays.js
+++ JavaScript/Scaler/02-Code-Execution-Hoisting/arrays.js
@@ -1,18 +1,18 @@
 var students = ["Kartik", "Namrata", "Saransh", "John", "Pankaj"];
-console.log(students);
-console.log(students.length);
+// console.log(students);
+// console.log(students.length);
 
-students.push("Sakshi");
-console.log(students);
+console.log(students.push("Sakshi"));
+// console.log(students);
 
-students.pop();
-console.log(students);
+console.log(students.pop());
+// console.log(students);
 
-students.shift();
-console.log(students);
+console.log(students.shift());
+// console.log(students);
 
-students.unshift("Sakshi");
-console.log(students);
+console.log(students.unshift("Sakshi"));
+// console.log(students);
 
 console.log(students.splice(2, 1));
-console.log(students);
+// console.log(students);
