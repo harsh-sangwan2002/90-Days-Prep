bd44c3996904dd5ba1d1305555037830d8771ea9
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-20 14:40:37 +0530

    Class 11 - Transactions done

--- Core Subjects/DBMS/11-Transactions2/05-Deadlock/1.sql
+++ Core Subjects/DBMS/11-Transactions2/05-Deadlock/1.sql
@@ -6,6 +6,6 @@ set autocommit = 0;
 
 Set session transaction isolation level serializable;
 
-Select * from film where film_id = 2 for update;
-
 Select * from film where film_id = 1 for update;
+
+Select * from film where film_id = 2 for update;
