95e7b821ea40e9d6ebd084d7048da5136d8ec56b
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-02-20 09:33:41 +0530

    Class 11, 12 - Schema Design done

---
+++ Core Subjects/DBMS/11-Transactions2/03-Repeatable-Read/2.sql
@@ -0,0 +1,15 @@
+commit;
+
+use sakila;
+
+Select * from film where film_id = 15;
+
+show variables like 'transaction_isolation';
+
+set session transaction isolation level REPEATABLE READ;
+
+Update film set title = "Rep Read" where film_id = 15;
+
+Set autocommit = 0;
+
+rollback;
