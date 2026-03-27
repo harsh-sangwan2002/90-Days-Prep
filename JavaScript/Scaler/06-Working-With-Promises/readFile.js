a9fdb1e1915cbaf884240ed0e88fa683e81362c0
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-03-27 15:49:59 +0530

    Removed old code

--- JavaScript/Scaler/06-Working-With-Promises/readFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const filePromise = fs.promises.readFile('f1.txt', 'utf8');
-
-filePromise.then(data => {
-    console.log(data);
-}).catch(err => {
-    console.error('Error reading file:', err);
-});
