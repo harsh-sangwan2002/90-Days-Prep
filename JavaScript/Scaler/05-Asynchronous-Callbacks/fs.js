48e6abc2aa5e4b7afdfd7fd5712d182f633a2584
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-03-27 15:49:33 +0530

    Class 05 - Async Callbacks done

---
+++ JavaScript/Scaler/05-Asynchronous-Callbacks/fs.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+console.log("Before");
+
+fs.readFile('f1.txt', (err, data) => {
+
+    if (err)
+        console.log(err);
+
+    else
+        console.log(data.toString());
+})
+
+fs.readFile('f2.txt', (err, data) => {
+
+    if (err)
+        console.log(err);
+
+    else
+        console.log(data.toString());
+})
+
+console.log("After");
