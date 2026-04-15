4075367147be6e8d65dd4c2edeae052187f54fd8
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-04-15 15:02:56 +0530

    removed old code

--- JavaScript/Scaler/04-Functional-Programming/HOF/map.js
+++
@@ -1,5 +0,0 @@
-const transactions = [1000, 2000, 3000, 4000, 5000];
-const inrToUSD = 80;
-
-const amountInUSD = transactions.map(amount => amount / inrToUSD);
-console.log(amountInUSD);
--- JavaScript/Scaler/04-Functional-Programming/HOF/reduce.js
+++
@@ -1,4 +0,0 @@
-const transactions = [1000, 2000, 3000, 4000, 5000];
-
-const totalAmount = transactions.reduce((acc, ele) => acc + ele, 0);
-console.log(totalAmount);
