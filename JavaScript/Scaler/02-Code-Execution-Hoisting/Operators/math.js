e3dfb66c3a116dad4710f78d8bcfe1d91a64c0c2
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-04-23 14:55:10 +0530

    Class 02 - Code Execution and Hoisting done

---
+++ JavaScript/Scaler/02-Code-Execution-Hoisting/Operators/math.js
@@ -0,0 +1,25 @@
+// Rounds off to the previous integer
+console.log(Math.floor(2.4));
+console.log(Math.floor(2.5));
+console.log(Math.floor(2.6));
+
+console.log("-------------------------");
+
+// Rounds off to the next integer
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(2.5));
+console.log(Math.ceil(2.6));
+
+console.log("-------------------------");
+
+// Rounds off to the appropriate integer
+console.log(Math.round(2.4));
+console.log(Math.round(2.5));
+console.log(Math.round(2.6));
+
+console.log("-------------------------");
+
+// Removes the decimal part
+console.log(Math.trunc(2.4));
+console.log(Math.trunc(2.5));
+console.log(Math.trunc(2.6));
