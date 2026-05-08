1cb23fbe2f3770279ba46e0e750114eafba06976
harsh-sangwan2002 <hsangwan2002@gmail.com>
2026-05-08 10:29:12 +0530

    Class 07 - Aggregate Queries done

--- Core Subjects/DBMS/07-Aggregate-Queries/AggregateFunctions.sql
+++ Core Subjects/DBMS/07-Aggregate-Queries/AggregateFunctions.sql
@@ -1,7 +1,32 @@
 use sakila;
 
+/*
+for each row1 in A:
+    for each row2 in B:
+        if(cond1 is true)
+            ans.add(row1+row2)
+        
+for each row in ans:
+    if(con2 is true)
+        ans2.add(row)
+
+count_s_id = 0
+
+for each row in ans2:
+    if(row[s_id] is not null)
+        count_s_id++
+
+print(count_s_id)
+*/
+Select
+From A
+Join B
+On cond1
+Where cond2
+
 Select count(*) from film;
 
+-- Return all the rows from the film table
 Select count(1) from film;
 
 Select count("Grey Man") from film;
--- Core Subjects/DBMS/07-Aggregate-Queries/GroupBy.sql
+++ Core Subjects/DBMS/07-Aggregate-Queries/GroupBy.sql
@@ -18,3 +18,42 @@ Join Batches b
 On s.batch_id = b.id
 Where s.batch_id = 1
 Group By b.name;
+
+/*
+1. Having - allows you to filter groups
+2. Where - allows you to filter rows
+*/
+
+/*
+for each row1 in A:
+    for each row2 in B:
+        if(cond1 is true)
+            ans.add(row1+row2)
+
+for each row in ans:
+    if(cond2 is true)
+        ans2.add(row)
+
+// Group By
+Map<Group by col> b_name_groups
+
+for each row in ans:
+    b_name_groups[row[b_name]].insert(row)
+
+for each row in b_name_groups:
+    if(cond3 is true / Having clause)
+        print(b_name)
+*/
+Select count(s.id), b_name
+From Students s
+Join Batches b
+On s.b_id = b.id
+Group By b.name
+Having count(s._id) > 100;
+
+-- We cannot use Where after group by, it should come after From/Join
+
+/*
+From -> Join/On -> Where -> Group By -> Having -> Select
+-> Order By -> Limit -> Offset
+*/
